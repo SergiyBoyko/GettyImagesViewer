e64e1dc29f0d22ac8902687268bf2ff2c96276ef
SergiyBoyko <developer.sergiyboyko@gmail.com>
2018-02-07 00:37:42 +0200

    Added method addHistory

--- app/src/main/java/com/example/serhii/gettyimagesviewer/views/LoadImageView.java
+++ app/src/main/java/com/example/serhii/gettyimagesviewer/views/LoadImageView.java
@@ -1,8 +1,13 @@
 package com.example.serhii.gettyimagesviewer.views;
 
+import com.example.serhii.gettyimagesviewer.model.entities.Image;
+
 /**
  * Created by Serhii on 06.02.2018.
  */
 
 public interface LoadImageView extends BaseView {
+
+    void addHistory(Image image, String phrase);
+
 }
