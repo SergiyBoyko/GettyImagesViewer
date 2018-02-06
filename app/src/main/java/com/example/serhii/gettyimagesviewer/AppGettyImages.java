dc5a5e94e9c9b7f20e7ea0b66849c7b13a3680d5
SergiyBoyko <developer.sergiyboyko@gmail.com>
2018-02-06 18:48:21 +0200

    Added application class

---
+++ app/src/main/java/com/example/serhii/gettyimagesviewer/AppGettyImages.java
@@ -0,0 +1,10 @@
+package com.example.serhii.gettyimagesviewer;
+
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * Created by Serhii on 06.02.2018.
+ */
+
+public class AppGettyImages extends MultiDexApplication {
+}
