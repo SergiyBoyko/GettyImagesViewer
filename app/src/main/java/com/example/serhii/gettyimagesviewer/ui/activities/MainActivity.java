801ec39e5c5d3b5528b95030628c4c083441a651
SergiyBoyko <developer.sergiyboyko@gmail.com>
2018-02-06 22:08:18 +0200

    Added searchView at MainActivity

--- app/src/main/java/com/example/serhii/gettyimagesviewer/ui/activities/MainActivity.java
+++ app/src/main/java/com/example/serhii/gettyimagesviewer/ui/activities/MainActivity.java
@@ -1,15 +1,59 @@
 package com.example.serhii.gettyimagesviewer.ui.activities;
 
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.serhii.gettyimagesviewer.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+
+        SearchView mSearchView = (SearchView) MenuItemCompat.getActionView(menu.findItem(R.id.action_search));
+        //set search view edit text color
+        EditText searchEditText = (EditText) mSearchView.findViewById(android.support.v7.appcompat.R.id.search_src_text);
+        searchEditText.setTextColor(getResources().getColor(R.color.colorPrimary));
+        searchEditText.setHintTextColor(getResources().getColor(R.color.colorPrimary));
+        //set max width
+        mSearchView.setMaxWidth(Integer.MAX_VALUE);
+        //set edit listener
+        mSearchView.setOnQueryTextListener(this);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        showText("onQueryTextSubmit");
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        showText("onQueryTextChange");
+        return false;
+    }
+
+    private void showText(String text) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
     }
 }
