55aeb370f28c9c561959bc0ea0603c84671fec16
SergiyBoyko <developer.sergiyboyko@gmail.com>
2018-02-07 00:38:06 +0200

    Implemented LoadImagePresenter

--- app/src/main/java/com/example/serhii/gettyimagesviewer/presenters/LoadImagePresenter.java
+++ app/src/main/java/com/example/serhii/gettyimagesviewer/presenters/LoadImagePresenter.java
@@ -1,7 +1,19 @@
 package com.example.serhii.gettyimagesviewer.presenters;
 
 import com.example.serhii.gettyimagesviewer.model.ILoadImageDataSource;
+import com.example.serhii.gettyimagesviewer.model.entities.GettyImagesResponse;
+import com.example.serhii.gettyimagesviewer.model.entities.Image;
+import com.example.serhii.gettyimagesviewer.utils.rx.RxErrorAction;
+import com.example.serhii.gettyimagesviewer.utils.rx.RxRetryWithDelay;
 import com.example.serhii.gettyimagesviewer.views.LoadImageView;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by Serhii on 06.02.2018.
@@ -12,5 +24,20 @@ public class LoadImagePresenter extends BasePresenter<LoadImageView> {
 
     public LoadImagePresenter(ILoadImageDataSource loadImageDataSource) {
         this.loadImageDataSource = loadImageDataSource;
+    }
+
+    public void getFeaturedContent(String phrase) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Api-Key", "qjru52ej5mc9rjffvm2rnftd");
+        addSubscription(loadImageDataSource.getBestContent(headers, phrase)
+                .retryWhen(new RxRetryWithDelay())
+                .map(GettyImagesResponse::getImages)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(images -> {
+                    if (images.size() > 0) getView().addHistory(images.get(0), phrase);
+                    else getView().addHistory(null, phrase);
+                }, new RxErrorAction(getView().getContext()))
+        );
     }
 }
