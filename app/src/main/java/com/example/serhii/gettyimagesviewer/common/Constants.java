c32ae077c61d32faf19244c3c709d8c9b8a1691e
SergiyBoyko <developer.sergiyboyko@gmail.com>
2018-02-06 18:53:05 +0200

    Added Constants class

---
+++ app/src/main/java/com/example/serhii/gettyimagesviewer/common/Constants.java
@@ -0,0 +1,11 @@
+package com.example.serhii.gettyimagesviewer.common;
+
+/**
+ * Created by Serhii on 06.02.2018.
+ */
+
+public class Constants {
+
+    public static final String GETTY_IMAGES_API = "https://api.gettyimages.com/";
+
+}
