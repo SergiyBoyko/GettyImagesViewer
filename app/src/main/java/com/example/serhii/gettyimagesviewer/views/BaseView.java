53e61498b0ca3a322820bf028e89ee3da605f838
SergiyBoyko <developer.sergiyboyko@gmail.com>
2018-02-07 00:10:24 +0200

    Added base view

---
+++ app/src/main/java/com/example/serhii/gettyimagesviewer/views/BaseView.java
@@ -0,0 +1,13 @@
+package com.example.serhii.gettyimagesviewer.views;
+
+import android.content.Context;
+
+/**
+ * Created by Serhii on 06.02.2018.
+ */
+
+public interface BaseView {
+
+    Context getContext();
+
+}
