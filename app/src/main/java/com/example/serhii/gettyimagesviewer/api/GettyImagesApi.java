b3782106c9eb48c2c1ae0212419dff30355149ac
SergiyBoyko <developer.sergiyboyko@gmail.com>
2018-02-06 23:58:39 +0200

    Added api method

--- app/src/main/java/com/example/serhii/gettyimagesviewer/api/GettyImagesApi.java
+++ app/src/main/java/com/example/serhii/gettyimagesviewer/api/GettyImagesApi.java
@@ -1,8 +1,22 @@
 package com.example.serhii.gettyimagesviewer.api;
 
+import com.example.serhii.gettyimagesviewer.model.entities.GettyImagesResponse;
+
+import java.util.Map;
+
+import retrofit2.http.GET;
+import retrofit2.http.HeaderMap;
+import retrofit2.http.Query;
+import rx.Observable;
+
 /**
  * Created by Serhii on 06.02.2018.
  */
 
 public interface GettyImagesApi {
+
+    @GET("/search/images?ﬁelds=id,title,thumb&sort_order=best")
+    Observable<GettyImagesResponse> getBestContent(@HeaderMap Map<String, String> headers,
+                                                   @Query("phrase") int phrase);
+
 }
