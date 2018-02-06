ef4de5bd08a77e51b8dbf7b3142ace65f5dd8252
SergiyBoyko <developer.sergiyboyko@gmail.com>
2018-02-06 23:32:08 +0200

    Added dagger builder in app class

--- app/src/main/java/com/example/serhii/gettyimagesviewer/AppGettyImages.java
+++ app/src/main/java/com/example/serhii/gettyimagesviewer/AppGettyImages.java
@@ -2,9 +2,30 @@ package com.example.serhii.gettyimagesviewer;
 
 import android.support.multidex.MultiDexApplication;
 
+import com.example.serhii.gettyimagesviewer.di.component.AppComponent;
+import com.example.serhii.gettyimagesviewer.di.component.DaggerAppComponent;
+import com.example.serhii.gettyimagesviewer.di.module.ApiModule;
+import com.example.serhii.gettyimagesviewer.di.module.AppModule;
+
 /**
  * Created by Serhii on 06.02.2018.
  */
 
 public class AppGettyImages extends MultiDexApplication {
+
+    private AppComponent appComponent;
+
+    public AppGettyImages() {
+        super();
+
+        appComponent = DaggerAppComponent.builder()
+                .apiModule(new ApiModule())
+                .appModule(new AppModule(this))
+                .build();
+    }
+
+    public AppComponent appComponent() {
+        return appComponent;
+    }
+
 }
