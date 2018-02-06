e033676fbc72ce425a95aa248be0c03528145e8d
SergiyBoyko <developer.sergiyboyko@gmail.com>
2018-02-06 18:43:55 +0200

    Main activity class was moved to ui.activities package

--- app/src/main/java/com/example/serhii/gettyimagesviewer/ui/activities/MainActivity.java
+++ app/src/main/java/com/example/serhii/gettyimagesviewer/ui/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.serhii.gettyimagesviewer;
+package com.example.serhii.gettyimagesviewer.ui.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.serhii.gettyimagesviewer.R;
 
 public class MainActivity extends AppCompatActivity {
 
