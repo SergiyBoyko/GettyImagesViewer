e473e44ee75ce1187dda2b31af14396b2fbdc045
SergiyBoyko <developer.sergiyboyko@gmail.com>
2018-02-07 11:52:01 +0200

    Refactored Image model class

--- app/src/main/java/com/example/serhii/gettyimagesviewer/model/entities/Image.java
+++ app/src/main/java/com/example/serhii/gettyimagesviewer/model/entities/Image.java
@@ -4,42 +4,23 @@ import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
 
-import io.realm.RealmObject;
-import io.realm.annotations.Ignore;
-import io.realm.annotations.Required;
-
 /**
  * Created by Serhii on 06.02.2018.
  */
 
-public class Image extends RealmObject {
-    @Ignore
+public class Image {
+    @SerializedName("id")
+    private String id;
+
     @SerializedName("display_sizes")
     private List<DisplaySize> displaySizes = null;
 
-    @Required
-    private String phrase;
-
-    @Required
-    private String url;
+    public String getId() {
+        return id;
+    }
 
     public List<DisplaySize> getDisplaySizes() {
         return displaySizes;
     }
 
-    public String getPhrase() {
-        return phrase;
-    }
-
-    public void setPhrase(String phrase) {
-        this.phrase = phrase;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
 }
