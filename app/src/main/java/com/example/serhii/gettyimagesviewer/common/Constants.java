7db01f4a0e04a9245148cb7761c098ec3b60392d
SergiyBoyko <developer.sergiyboyko@gmail.com>
2018-02-07 11:51:25 +0200

    Added constants

--- app/src/main/java/com/example/serhii/gettyimagesviewer/common/Constants.java
+++ app/src/main/java/com/example/serhii/gettyimagesviewer/common/Constants.java
@@ -6,8 +6,11 @@ package com.example.serhii.gettyimagesviewer.common;
 
 public class Constants {
 
-    public static final String GETTY_IMAGES_API = "https://api.gettyimages.com/v3/";
+    public static final String GETTY_IMAGES_API = "https://api.gettyimages.com";
 
     public static final String API_KEY = "qjru52ej5mc9rjffvm2rnftd";
+    public static final String TOKEN = "zxXnXwet7UYSxAhnsjyEgvBWNFsTdHev3mxPtsgRnpvxU";
+
+    public static final String HISTORY_DATE_PATTERN = "HH:mm dd/MM/yyyy";
 
 }
