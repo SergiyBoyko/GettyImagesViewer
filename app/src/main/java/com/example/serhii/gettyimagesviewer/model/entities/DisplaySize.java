15735d24ad47ed12b45e1c8e8dd777353c61efa1
SergiyBoyko <developer.sergiyboyko@gmail.com>
2018-02-07 13:18:38 +0200

    Removed unused libraries

--- app/src/main/java/com/example/serhii/gettyimagesviewer/model/entities/DisplaySize.java
+++ app/src/main/java/com/example/serhii/gettyimagesviewer/model/entities/DisplaySize.java
@@ -1,6 +1,5 @@
 package com.example.serhii.gettyimagesviewer.model.entities;
 
-import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 /**
--- app/src/main/java/com/example/serhii/gettyimagesviewer/widgets/adapters/HistoryListAdapter.java
+++ app/src/main/java/com/example/serhii/gettyimagesviewer/widgets/adapters/HistoryListAdapter.java
@@ -3,18 +3,13 @@ package com.example.serhii.gettyimagesviewer.widgets.adapters;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.resource.drawable.GlideDrawable;
-import com.bumptech.glide.request.RequestListener;
-import com.bumptech.glide.request.target.Target;
 import com.example.serhii.gettyimagesviewer.R;
 import com.example.serhii.gettyimagesviewer.model.entities.HistoryElement;
-import com.example.serhii.gettyimagesviewer.model.entities.Image;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
